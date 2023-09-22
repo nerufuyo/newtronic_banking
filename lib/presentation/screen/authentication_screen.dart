ed0a9ca29bf1cd057765380c508a95f8eb6f669f
nerufuyo <listyoadipamungkas1401@gmail.com>
2023-09-22 21:15:06 +0700

    🖥️ wip: introduction screen

---
+++ lib/presentation/screen/authentication_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AuthenticationScreen extends StatefulWidget {
+  const AuthenticationScreen({super.key});
+  static const routeName = '/authentication';
+
+  @override
+  State<AuthenticationScreen> createState() => _AuthenticationScreenState();
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/presentation/screen/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  static const routeName = '/splash';
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: SvgPicture.asset(
+            'lib/assets/images/logo.svg',
+            height: 100,
+            width: 100,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
